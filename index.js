4accc9f454f3d737c1edc94493a9dec921e59cf3
manoman6565-lgtm <manoman6565@gmail.com>
2026-06-08 02:27:17 +0300

    بوت الملك

--- index.js
+++ index.js
@@ -33,13 +33,13 @@ if (!global.db) {
 /* =========== Config ========== */
 const { config } = client;
 config.info = { 
-  nameBot: "♡ 𝙋𝙊𝙈𝙉𝙄 🎪 〈", 
-  nameChannel: "𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️", 
+  nameBot: "♡ بوت الملك اڪازا 〈", 
+  nameChannel: "اݪــٚـٖٖٖٜ̐ـ͜ۥــٚـمـ͇͡ــݪــٚـ‹̷ٰٖ٭̷ٰٖ›ــٚـك اڪـ⃪ﹻـٰٰٰٖٖٖٜ۬ـٰٰٰٖٖٜـ⃪᭓⃟ـ⃪ـازا", 
   idChannel: "120363225356834044@newsletter",
   urls: {
     repo: "https://github.com/deveni0/Pomni-AI",
     api: "https://emam-api.web.id",
-    channel: "https://whatsapp.com/channel/0029VaQim2bAu3aPsRVaDq3v"
+    channel: "https://whatsapp.com/channel/0029VbDIbxLInlqJIE7XDM34"
   },
   copyright: { 
     pack: 'ڤـ ـ VA ـ ـا', 
